15bae75338ea3a2649c513706a8c8208692f7dec
sofi varela <sofia.varela@mercadolibre.com>
2020-12-12 15:28:06 -0300

    Add car insurance config

---
+++ config/carInsurances.js
@@ -0,0 +1,13 @@
+const FULL_COVERAGE = 'Full Coverage';
+const MEGA_COVERAGE = 'Mega Coverage';
+const SPECIAL_FULL_COVERAGE = 'Special Full Coverage';
+const SUPER_SALE = 'Super Sale';
+const OTHERS = 'Otros productos'
+
+module.exports = {
+  FULL_COVERAGE,
+  MEGA_COVERAGE,
+  SPECIAL_FULL_COVERAGE,
+  SUPER_SALE,
+  OTHERS
+};
